316d94e685fc58abc316b6d5a31836b5f9c7ea96
Antoni S. Puimedon <asegurap@redhat.com>
2013-02-23 23:30:53 +0100

    Add network definitions for bridged nets.

---
+++ setupNetworks_fromEng_br.py
@@ -0,0 +1,15 @@
+{'black':
+     {'nic': 'eth3',
+      'vlan': '4',
+      'STP': 'no',
+      'bridged': 'true'
+     },
+ 'green':
+     {'nic': 'eth3',
+      'vlan': '5',
+      'STP': 'no',
+      'bridged': 'true'
+     }
+},
+{},
+{'connectivityCheck': 'true', 'connectivityTimeout': 120}
